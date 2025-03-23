b9a24cb26fe519821b5d2184e2ad5e9c57a1b6b4
mrmtsu <81202muramatsu@gmail.com>
2025-03-24 06:46:00 +0900

    add Mux integration for video uploads and implement responsive upload modal

--- src/app/modules/videos/server/prrocedures.ts
+++ src/app/modules/videos/server/prrocedures.ts
@@ -1,10 +1,19 @@
 import { db } from "@/db";
 import { videos } from "@/db/schema";
+import { mux } from "@/lib/mux";
 import { protectedProcedure, createTRPCRouter } from "@/trpc/init";
 
 export const videosRouter = createTRPCRouter({
   create: protectedProcedure.mutation(async ({ ctx }) => {
     const { id: userId } = ctx.user;
+
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        passthrough: userId,
+        playback_policy: ["public"],
+      },
+      cors_origin: "*",
+    });
 
     const [video] = await db
       .insert(videos)
@@ -12,11 +21,14 @@ export const videosRouter = createTRPCRouter({
         userId,
         title: "Untitled",
         description: "",
+        muxStatus: "waiting",
+        muxUploadId: upload.id,
       })
       .returning();
 
     return {
       video: video,
+      url: upload.url,
     };
   }),
 });
